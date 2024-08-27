27d4dc15e8400c26bbed5021ed8a3e79c26dbe9c
Labonno <shanjidaislam0184@gmail.com>
2024-08-27 14:48:09 +0600

    Add Test Data Builder for streamlined test data creation

---
+++ src/test/java/testdata/TestDataBuilder.java
@@ -0,0 +1,18 @@
+package testdata;
+import net.datafaker.Faker;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+public class TestDataBuilder {
+
+    public static BookingData getBookingData(String firstName, String lastName, int price, boolean depositPaid, BookingDates bookingDates, String meal) {
+        return new BookingData(firstName, lastName, price, depositPaid, bookingDates, meal);
+    }
+
+    public static AuthData getAuthData(String username, String password) {
+        return new AuthData(username, password);
+    }
+
+    public static PartialUpdateBookingData getPartialUpdateBookingData(int price, String fruit) {
+        return new PartialUpdateBookingData(price, fruit);
+    }
+}
